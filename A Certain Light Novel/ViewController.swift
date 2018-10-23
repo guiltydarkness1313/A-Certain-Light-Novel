4d9b95069655c95919a47238d757804005d3b73b
Paul Pacheco <paul.frankpc@gmail.com>
2018-10-23 17:21:03 -0500

    Initial Commit

---
+++ A Certain Light Novel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A Certain Light Novel
+//
+//  Created by Paul Pacheco on 10/23/18.
+//  Copyright © 2018 ShibuyaXpress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
